f9a7df953d1c1a600ecf191628e79b49b0a5b861
moussasg <117439847+moussasg@users.noreply.github.com>
2023-05-02 00:12:40 +0100

    avancé

--- express.js
+++ express.js
@@ -1,46 +1,87 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-const app = express();
-app.use(bodyParser.json());
-// Connect to the database
-mongoose.connect('mongodb+srv://moussasouagg:1Yzsk2SKlJMxbSgW@cluster0.lnp5clj.mongodb.net/?retryWrites=true&w=majority', {
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'; // framework de node.js gére les requêtes et les réponses HTTP , facilite création de serveur web
+import bodyParser from 'body-parser'; //  bibliothèque middleware pour Express.js pour 
+///récupérer les données envoyées dans le corps d'une requête HTTP et le transformer en objet 
+import mongoose from 'mongoose'; /// driver de mongodb dans node.js pour définir des shémas pour faciliter les opération qui travaille avec node.js
+const app = express(); /// cluster0.lnp5clj.mongodb.net nom de cluster
+app.use(bodyParser.json()); // 1Yzsk2SKlJMxbSgW c le mdp sur database atlas
+// Connect to the database // moussasouagg c le username de database sur atlas 
+mongoose.connect( process.env.Db_Connection , {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-// Define the user schema and model
-const userSchema = new mongoose.Schema({
+}).then(() => {
+  console.log('Connected to database');
+}).catch((error) => {
+  console.log('Unable to connect to database', error);
+})
+// Define the userschema ndiroh aprés dakhel model 
+const userSchema = new mongoose.Schema({ 
   name: String,
   email: String,
   password: String,
 });
 let User;
-try {
-  User = mongoose.model('users');
-} catch {
-  User = mongoose.model('users', userSchema);
-}
-// Route for receiving form data
-app.post('/api/users', async (req, res) => {
+User = mongoose.model('User', userSchema);
+// Route for receiving form data pour que la méthode post marche
+app.post('/api/users', async (req, res) => { // users = collection
   const { name, email, password } = req.body;
   try {
     const user = new User({ name, email, password });
     await user.save();
-    console.log(`Data inserted into "users" collection with ID ${user._id}`);
+    console.log(`name :  ${name} , email :${email} , password : ${password} / inserted into "users" collection with ID ${user._id}`);
     res.json({ message: 'Form data saved successfully' });
   } catch (err) {
     console.log(err);
     res.status(500).send('Error inserting data into database');
-  }
+  } } )
+  /// get all users
+  app.get('/api/users', async (req, res) => {
+    try {
+      const users = await User.find({});
+      res.json(users);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send('Error getting users');
+    }
+  });
+  // Route to get specific id
+  app.get('/api/users/:id', async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      if (!user) {
+        res.status(404).send('User not found');
+        return;
+      }
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send('Error retrieving data from database');
+    }
+  });
+  app.delete('/api/users/:id', async (req, res) => {
+    const userId = req.params.id;
+    try {
+      const deletedUser = await User.findByIdAndDelete(userId);
+      if (deletedUser) {
+        res.json({ message: `User with ID ${userId} deleted successfully` });
+      } else {
+        res.status(404).json({ message: `User with ID ${userId} not found` });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).send('Error deleting user from database');
+    }
+  });
+  
+// Start the server
+app.listen(3001, () => {
+  console.log('Server started on port 3001');
 });
+///
 // Middleware for CORS requests
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
-});
-// Start the server
-app.listen(3001, () => {
-  console.log('Server started on port 3001');
 });
--- src/pages/signin/Signin.jsx
+++ src/pages/signin/Signin.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import axios from "axios";
+import axios from "axios"; //bibliothèque JavaScript envoie les requette http au serveur
+//UserForm classe React avec un état initial contient des propriétés pour stocker 
+//le nom, l'e-mail et le mot de passe 
 class UserForm extends React.Component {
-  constructor(props) { // get 3ando param yjo f lien 
-    super(props); // post param + y9bl body 
+constructor(props) { //constructeur initialise l'état de la classe avec un objet props et apelle React.compoenet
+    super(props); 
     this.state = {
       name: '',
       email: '',
       password: ''
     };
-  }
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const { name, email, password } = this.state; /// comunikit react-node avec lien node li howa http://localhost:4000
+  } // handleSubmit
+handleSubmit = (event) => {//tetactiva en cliquand sur submit de form au dessous
+    event.preventDefault(); // 3001 = PORT
+    /// name + email + password rahi fel form au dessous
+    const { name, email, password } = this.state; /// comunikit react-node avec axios li yb3te request b lien node li howa http://localhost:4000
     axios.post('http://localhost:3001/api/users/', { name, email, password }) // hna n7aAT LIEN NODE__ li moraha standard/api/users/
       .then((response) => {
         console.log(response.data);
@@ -19,7 +22,7 @@ class UserForm extends React.Component {
         this.setState({
           name: '',
           email: '',
-          password: ''
+          password: '',
         });
       })
       .catch((error) => {
